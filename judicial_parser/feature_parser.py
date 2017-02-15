8bf2c458939050b3e7d4b527a77bd50db82a45cd
Jinyong Shin <jinyongshin@Jinyongs-MacBook-Air.local>
2017-02-15 02:04:03 -0500

    feature_parser.py

--- judicial_parser/feature_parser.py
+++ judicial_parser/feature_parser.py
@@ -1,5 +1,6 @@
-# Author : Jin Yong Shin
+# Author : Jin Yong Shin 
 # Date : 2017.02.09
+# @copyright : Jin Yong Shin
 
 # Purpose : First phase of parsing text file that is converted from PDF
 # Might contains some unnecessary spacings
@@ -26,7 +27,7 @@ class text_converter():
 
 	def parse_main(self):
 		csv_file = open("phase1.csv", 'w')
-		fieldnames = ['filename', 'type', 'date', 'company info']
+		fieldnames = ['circuit_no', 'type', 'file_date', 'dates', 'company info']
 		writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
 		writer.writeheader()
 		for file_path in self.file_list:
@@ -34,19 +35,30 @@ class text_converter():
 			#parse date from txt
 			judge_date = self.parse_date(file_object)
 			#parse apelle
-			file_object = open(file_path, 'rb')
+			file_object.seek(0)
 			apelle_info = self.parse_apellee(file_object)
 			#parse type of course appeals
-			file_object = open(file_path, 'rb')
+			file_object.seek(0)
 			court_type = self.parse_type(file_object)
-			writer.writerow({'filename' : file_path, "type" : court_type, "date" : judge_date, "company info" : apelle_info})
+
+			case_num = self.parse_case_no(file_path)
+
+			#file date
+			file_date = self.parse_filedate(file_path)
+
+			writer.writerow({'circuit_no' : case_num, "type" : court_type, 'file_date' : file_date, "dates" : judge_date, "company info" : apelle_info})
 
 
 	def parse_date(self, file_object):
-		# text = file_object.read()
-		# match = re.findall(r'(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}', text)
-		# return (match)
-		
+		text = file_object.read()
+		match = re.findall(r'(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}', text)
+		file_object.seek(0)
+		# for line in file_object:
+		# 	line_data = line.split('\n')[0].strip()
+		# 	for element in match:
+		# 		if element in line_data:
+
+		return match			
 
 
 	def parse_apellee(self, file_object):
@@ -59,9 +71,11 @@ class text_converter():
 				if line_data != '' and line_data != 'v.':
 					data_to_store.append(line_data)
 			if len(data_to_store) > 0:
-				if re.match("Defendants", data_to_store[-1]) or re.match("Defendant", data_to_store[-1]):
+				if re.match("Defendant", data_to_store[-1]):
 					return data_to_store
-				elif re.match("Appellee", data_to_store[-1]) or re.match("Appellees", data_to_store[-1]):
+				elif re.match("Appellee", data_to_store[-1]):
+					return data_to_store
+				elif re.match("Appellant", data_to_store[-1]):
 					return data_to_store
 					
 	def parse_type(self, file_object):
@@ -81,6 +95,15 @@ class text_converter():
 			if re.match("^[A-Z]", word):
 				return_val += word + " "
 		return return_val
+
+	def parse_case_no(self, file_name):
+		match = re.findall(r'\d{2,4}-\d{4}', file_name)
+		return match[0]
+
+	def parse_filedate(self, file_name):
+		match = re.findall(r'\d{1,2}-\d{2,4}-\d{4}', file_name)
+		return match[0]
+
 
 def main():
 	convt = text_converter("Lists/list1.txt")
